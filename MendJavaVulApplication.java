74f7c6cee8b1823e5a0e4409127d60d9f454e55c
aviatam-mend <149406890+aviatam-mend@users.noreply.github.com>
2025-03-20 15:32:22 +0200

    Add files via upload

---
+++ MendJavaVulApplication.java
@@ -0,0 +1,13 @@
+package io.mend.remediation.mendjavavul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MendJavaVulApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MendJavaVulApplication.class, args);
+  }
+
+}
